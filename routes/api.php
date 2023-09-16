9588444bc5d232cba423d0fd915e3d970e854ade
jogjatanpakamu <aaamar.er@gmail.com>
2023-09-16 15:55:46 +0700

    add todos mc

--- routes/api.php
+++ routes/api.php
@@ -14,4 +14,11 @@ Route::controller(AuthController::class)->group(function () {
 });
 
 
-Route::get('me', fn () => response()->json(['user' => auth()->user()]))->middleware('api');
+
+Route::controller(TodoController::class)->group(function () {
+    Route::get('todos', 'index');
+    Route::post('todo', 'store');
+    Route::get('todo/{id}', 'show');
+    Route::put('todo/{id}', 'update');
+    Route::delete('todo/{id}', 'destroy');
+});
